2a92670b2aa4f4858ab8189e9fcb9831e82be06a
Gwilym  Berrow <gwilymberrow2000@gmail.com>
2024-09-23 14:56:27 +0000

    Added comments for the js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
+// Toggle the mobile menu when the hamburger icon is clicked
 function toggleMenu() {
     const menu = document.querySelector(".menu-links")
     const icon = document.querySelector(".hamburger-icon")
+
+    // Toggle the 'open' class on both the menu and icon for animation
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
