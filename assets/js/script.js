ab6add3eacaec05f9d333cec45a57dc316c6db8c
Gwilym  Berrow <gwilymberrow2000@gmail.com>
2024-09-25 11:10:04 +0000

    Added hamburger menu toggle for mobile navigation

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,15 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+// Dark / Light mode
+
+const btn = document.getElementById("modeToggle");
+const btn2 = document.getElementById("modeToggle2");
+const themeIcons =  document.querySelectorAll(".icon");
+const currentTheme = localStorage.getItem("theme");
+
+if (currentTheme === "dark") {
+    setDarkMode(); 
+}
+
