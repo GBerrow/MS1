e7c7349aeb26050dc7c54e64b060028ccf0eb887
Gwilym  Berrow <gwilymberrow2000@gmail.com>
2024-09-25 12:01:45 +0000

    Added comments on js to explain each section

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,17 +8,22 @@ function toggleMenu() {
   icon.classList.toggle("open");
 }
 
-// Dark / Light mode
-
+// Get the mode toggle buttons from the DOM
 const btn = document.getElementById("modeToggle");
 const btn2 = document.getElementById("modeToggle2");
+
+// Get all elements with the "icon" class to change their source (light/dark) dynamically
 const themeIcons = document.querySelectorAll(".icon");
+
+// Retrieve the current theme from localStorage (if the user has already selected a theme)
 const currentTheme = localStorage.getItem("theme");
 
+// If the current theme is dark, apply dark mode styles on page load
 if (currentTheme === "dark") {
   setDarkMode();
 }
 
+// Add event listeners to the desktop and mobile theme toggle buttons
 btn.addEventListener("click", function () {
   setTheme();
 });
@@ -27,9 +32,11 @@ btn2.addEventListener("click", function () {
   setTheme();
 });
 
+// Function to toggle between dark and light modes
 function setTheme() {
   let currentTheme = document.body.getAttribute("theme");
 
+  // If the current theme is dark, switch to light mode; otherwise, switch to dark mode
   if (currentTheme === "dark") {
     setLightMode();
   } else {
@@ -37,6 +44,7 @@ function setTheme() {
   }
 }
 
+// Function to apply dark mode
 function setDarkMode() {
   document.body.setAttribute("theme", "dark");
   localStorage.setItem("theme", "dark");
@@ -46,6 +54,7 @@ function setDarkMode() {
   });
 }
 
+// Function to apply light mode
 function setLightMode() {
   document.body.removeAttribute("theme");
   localStorage.setItem("theme", "light");
