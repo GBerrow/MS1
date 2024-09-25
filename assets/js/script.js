26c8cd0f1480646bbd0ed52c4792568e0e976fd0
Gwilym  Berrow <gwilymberrow2000@gmail.com>
2024-09-25 11:32:14 +0000

    Completed dark/light mode functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,4 +37,20 @@ function setTheme() {
     }
 }
 
+function setDarkMode() {
+    document.body.setAttribute("theme", "dark");
+    localStorage.setItem("theme", "dark");
 
+    themeIcons.forEach((icons) => {
+       icon.src = icon.getAttribute("src-dark");
+    });
+}
+
+function setLightMode() {
+    document.body.setAttribute("theme");
+    localStorage.setItem("theme", "light");
+
+    themeIcons.forEach((icons) => {
+       icon.src = icon.getAttribute("src-light");
+    });
+}
